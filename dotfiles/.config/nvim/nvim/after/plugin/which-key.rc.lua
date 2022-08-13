219991f8ab23f3eb3bc7fe2cc77b0150068a8e1b
paolo banez <paolo.banez@intechsive.com>
2022-08-14 03:29:29 +0800

    update nvim config

---
+++ dotfiles/.config/nvim/nvim/after/plugin/which-key.rc.lua
@@ -0,0 +1,53 @@
+local status, which_key = pcall(require, 'which-key')
+if (not status) then return end
+
+which_key.setup {}
+
+which_key.register({
+  [";"] = {
+    f = "Find files",
+    r = "Live grep",
+    t = "Help tags",
+    e = "Diagnostics",
+    [";"] = "Resume Telescope",
+  },
+  ["\\"] = {
+    ["\\"] = "Show all buffers"
+  },
+  ["<leader>"] = {
+    hs = "Stage hunk",
+    hr = "Reset hunk",
+    hS = "Stage buffer",
+    hR = "Reset buffer",
+    hu = "Undo staged hunk",
+    hp = "Preview hunk",
+    hb = "Git blame line",
+    tb = "Toggle current line blame",
+    hd = "Diff this file",
+    hD = "Diff this <>",
+    td = "Toggle deleted",
+  },
+  s = {
+    f = "File browser",
+    s = "Split window",
+    v = "Vertical split window",
+    h = "Move to left window",
+    l = "Move to right window",
+    j = "Move down window",
+    k = "Move up window",
+    ["<left>"] = "Move to left window",
+    ["<right>"] = "Move to right window",
+    ["<down>"] = "Move down window",
+    ["<up>"] = "Move up window",
+  },
+  g = {
+    D = "Jumps to the declaration",
+    i = "List all implementations",
+    d = "Find files using variable",
+    p = "Preview definition",
+    r = "Rename variable"
+  },
+  t = {
+    e = "New tab"
+  }
+}, {})
---
+++ dotfiles/.config/nvim/nvim/lua/plugins.lua
@@ -0,0 +1,49 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then print("Packer is not installed")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+
+  use 'kyazdani42/nvim-web-devicons' -- File icons
+  use 'glepnir/lspsaga.nvim' -- LSP UIs
+  use 'L3MON4D3/LuaSnip' -- Snippet
+  use 'terrortylor/nvim-comment' -- Commenter
+  -- Statusline
+  use 'nvim-lualine/lualine.nvim'
+  -- LSP
+  use 'williamboman/nvim-lsp-installer'
+  use "b0o/schemastore.nvim" -- jsonschema
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'hrsh7th/cmp-path' -- nvim-cmp auto complete paths
+  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's build in LSP
+  use 'hrsh7th/nvim-cmp' -- Completion
+  use 'neovim/nvim-lspconfig'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as language server to inject LSP diagnostics
+  use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
+
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+
+  use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+
+  use 'akinsho/nvim-bufferline.lua'
+  use 'norcalli/nvim-colorizer.lua'
+
+  use 'lewis6991/gitsigns.nvim'
+  use 'folke/which-key.nvim'
+end)
